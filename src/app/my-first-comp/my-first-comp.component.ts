e3d21ef0366d18b19a5f0854e42a4f4e1b8ffee6
uakubue <uakubue@gmail.com>
2024-02-22 17:10:02 +0100

    first commit

---
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-first-comp',
+  templateUrl: './my-first-comp.component.html',
+  styleUrl: './my-first-comp.component.scss'
+})
+export class MyFirstCompComponent {
+
+  name: string = "";
+  email: string = "";
+  message: string = "";
+  isSubmitted: boolean = false;
+  messages: Array<any> = [];
+
+  onSubmit() {
+    this.isSubmitted = true;
+    this.messages.push({
+      'name': this.name,
+      'email': this.email,
+      'message': this.message
+    });
+    console.log(this.messages);
+  }
+
+}
